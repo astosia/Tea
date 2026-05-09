234efbafa5908466f0ba877162acea1a06da1f0c
astosia <39700835+astosia@users.noreply.github.com>
2026-05-09 22:19:32 +0100

    Delete src/tea_cup.h

--- src/tea_cup.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <pebble.h>
-
-/********************/
-/*     FUNCTION     */
-/********************/
-
-void tea_cup_destroy();
-void tea_cup_draw(Layer*, GContext*, uint8_t, bool);
